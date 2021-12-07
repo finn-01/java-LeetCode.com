e5473570ec68264d63db88753e8708c3bf119f2f
samukma <nguyensam576@gmail.com>
2021-12-07 20:40:11 +0700

    roman to integer c1

---
+++ src/easy/RomanToInteger.java
@@ -0,0 +1,60 @@
+package easy;
+
+public class RomanToInteger {
+    public int romanToInt(String s){
+        int[] nums = new int[s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+            switch (s.charAt(i)){
+                case 'M':
+                    nums[i] = 1000;
+                    break;
+
+                case 'D':
+                    nums[i] = 500;
+                    break;
+
+                case 'C':
+                    nums[i] = 100;
+                    break;
+
+                case 'L':
+                    nums[i] = 50;
+                    break;
+
+                case 'X':
+                    nums[i] = 10;
+                    break;
+
+                case 'V':
+                    nums[i] = 5;
+                    break;
+
+                case 'I':
+                    nums[i] = 1;
+                    break;
+            }
+        }
+
+        int sum = 0;
+
+        for(int i = 0; i < nums.length - 1; i++){
+
+            //Neu la IX (vi du)
+            if(nums[i] < nums[nums.length - 1]){
+                sum -= nums[i];
+            }
+
+            //Neu la XI (vi du)
+            else {
+                sum += nums[i];
+            }
+        }
+        return sum + nums[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger romanToInteger = new RomanToInteger();
+        System.out.println(romanToInteger.romanToInt("XX"));
+    }
+}
